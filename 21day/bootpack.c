77138ac6ab79da96004bb1c82c2e9b502504e800
willinin <315793290@qq.com>
2016-12-10 12:34:23 +0800

    21day
    21day

---
+++ 21day/bootpack.c
@@ -0,0 +1,282 @@
+/* bootpack�̃��C�� */
+
+#include "bootpack.h"
+#include <stdio.h>
+
+#define KEYCMD_LED		0xed
+
+void HariMain(void)
+{
+	struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
+	struct SHTCTL *shtctl;
+	char s[40];
+	struct FIFO32 fifo, keycmd;
+	int fifobuf[128], keycmd_buf[32];
+	int mx, my, i, cursor_x, cursor_c;
+	unsigned int memtotal;
+	struct MOUSE_DEC mdec;
+	struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
+	unsigned char *buf_back, buf_mouse[256], *buf_win, *buf_cons;
+	struct SHEET *sht_back, *sht_mouse, *sht_win, *sht_cons;
+	struct TASK *task_a, *task_cons;
+	struct TIMER *timer;
+	static char keytable0[0x80] = {
+		0,   0,   '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '^', 0,   0,
+		'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '@', '[', 0,   0,   'A', 'S',
+		'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', ':', 0,   0,   ']', 'Z', 'X', 'C', 'V',
+		'B', 'N', 'M', ',', '.', '/', 0,   '*', 0,   ' ', 0,   0,   0,   0,   0,   0,
+		0,   0,   0,   0,   0,   0,   0,   '7', '8', '9', '-', '4', '5', '6', '+', '1',
+		'2', '3', '0', '.', 0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+		0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+		0,   0,   0,   0x5c, 0,  0,   0,   0,   0,   0,   0,   0,   0,   0x5c, 0,  0
+	};
+	static char keytable1[0x80] = {
+		0,   0,   '!', 0x22, '#', '$', '%', '&', 0x27, '(', ')', '~', '=', '~', 0,   0,
+		'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '`', '{', 0,   0,   'A', 'S',
+		'D', 'F', 'G', 'H', 'J', 'K', 'L', '+', '*', 0,   0,   '}', 'Z', 'X', 'C', 'V',
+		'B', 'N', 'M', '<', '>', '?', 0,   '*', 0,   ' ', 0,   0,   0,   0,   0,   0,
+		0,   0,   0,   0,   0,   0,   0,   '7', '8', '9', '-', '4', '5', '6', '+', '1',
+		'2', '3', '0', '.', 0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+		0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+		0,   0,   0,   '_', 0,   0,   0,   0,   0,   0,   0,   0,   0,   '|', 0,   0
+	};
+	int key_to = 0, key_shift = 0, key_leds = (binfo->leds >> 4) & 7, keycmd_wait = -1;
+
+	init_gdtidt();
+	init_pic();
+	io_sti(); /* IDT/PIC�̏��������I�������̂�CPU�̊��荞�݋֎~������ */
+	fifo32_init(&fifo, 128, fifobuf, 0);
+	init_pit();
+	init_keyboard(&fifo, 256);
+	enable_mouse(&fifo, 512, &mdec);
+	io_out8(PIC0_IMR, 0xf8); /* PIT��PIC1�ƃL�[�{�[�h������(11111000) */
+	io_out8(PIC1_IMR, 0xef); /* �}�E�X������(11101111) */
+	fifo32_init(&keycmd, 32, keycmd_buf, 0);
+
+	memtotal = memtest(0x00400000, 0xbfffffff);
+	memman_init(memman);
+	memman_free(memman, 0x00001000, 0x0009e000); /* 0x00001000 - 0x0009efff */
+	memman_free(memman, 0x00400000, memtotal - 0x00400000);
+
+	init_palette();
+	shtctl = shtctl_init(memman, binfo->vram, binfo->scrnx, binfo->scrny);
+	task_a = task_init(memman);
+	fifo.task = task_a;
+	task_run(task_a, 1, 2);
+
+	/* sht_back */
+	sht_back  = sheet_alloc(shtctl);
+	buf_back  = (unsigned char *) memman_alloc_4k(memman, binfo->scrnx * binfo->scrny);
+	sheet_setbuf(sht_back, buf_back, binfo->scrnx, binfo->scrny, -1); /* �����F�Ȃ� */
+	init_screen8(buf_back, binfo->scrnx, binfo->scrny);
+
+	/* sht_cons */
+	sht_cons = sheet_alloc(shtctl);
+	buf_cons = (unsigned char *) memman_alloc_4k(memman, 256 * 165);
+	sheet_setbuf(sht_cons, buf_cons, 256, 165, -1); /* �����F�Ȃ� */
+	make_window8(buf_cons, 256, 165, "console", 0);
+	make_textbox8(sht_cons, 8, 28, 240, 128, COL8_000000);
+	task_cons = task_alloc();
+	task_cons->tss.esp = memman_alloc_4k(memman, 64 * 1024) + 64 * 1024 - 12;
+	task_cons->tss.eip = (int) &console_task;
+	task_cons->tss.es = 1 * 8;
+	task_cons->tss.cs = 2 * 8;
+	task_cons->tss.ss = 1 * 8;
+	task_cons->tss.ds = 1 * 8;
+	task_cons->tss.fs = 1 * 8;
+	task_cons->tss.gs = 1 * 8;
+	*((int *) (task_cons->tss.esp + 4)) = (int) sht_cons;
+	*((int *) (task_cons->tss.esp + 8)) = memtotal;
+	task_run(task_cons, 2, 2); /* level=2, priority=2 */
+
+	/* sht_win */
+	sht_win   = sheet_alloc(shtctl);
+	buf_win   = (unsigned char *) memman_alloc_4k(memman, 160 * 52);
+	sheet_setbuf(sht_win, buf_win, 144, 52, -1); /* �����F�Ȃ� */
+	make_window8(buf_win, 144, 52, "task_a", 1);
+	make_textbox8(sht_win, 8, 28, 128, 16, COL8_FFFFFF);
+	cursor_x = 8;
+	cursor_c = COL8_FFFFFF;
+	timer = timer_alloc();
+	timer_init(timer, &fifo, 1);
+	timer_settime(timer, 50);
+
+	/* sht_mouse */
+	sht_mouse = sheet_alloc(shtctl);
+	sheet_setbuf(sht_mouse, buf_mouse, 16, 16, 99);
+	init_mouse_cursor8(buf_mouse, 99);
+	mx = (binfo->scrnx - 16) / 2; /* ���ʒ����ɂȂ��悤�ɍ��W�v�Z */
+	my = (binfo->scrny - 28 - 16) / 2;
+
+	sheet_slide(sht_back,  0,  0);
+	sheet_slide(sht_cons, 32,  4);
+	sheet_slide(sht_win,  64, 56);
+	sheet_slide(sht_mouse, mx, my);
+	sheet_updown(sht_back,  0);
+	sheet_updown(sht_cons,  1);
+	sheet_updown(sht_win,   2);
+	sheet_updown(sht_mouse, 3);
+
+	/* �ŏ��ɃL�[�{�[�h���ԂƂ̐H���Ⴂ���Ȃ��悤�ɁA�ݒ肵�Ă������Ƃɂ��� */
+	fifo32_put(&keycmd, KEYCMD_LED);
+	fifo32_put(&keycmd, key_leds);
+
+	for (;;) {
+		if (fifo32_status(&keycmd) > 0 && keycmd_wait < 0) {
+			/* �L�[�{�[�h�R���g���[���ɑ����f�[�^�������΁A���� */
+			keycmd_wait = fifo32_get(&keycmd);
+			wait_KBC_sendready();
+			io_out8(PORT_KEYDAT, keycmd_wait);
+		}
+		io_cli();
+		if (fifo32_status(&fifo) == 0) {
+			task_sleep(task_a);
+			io_sti();
+		} else {
+			i = fifo32_get(&fifo);
+			io_sti();
+			if (256 <= i && i <= 511) { /* �L�[�{�[�h�f�[�^ */
+				if (i < 0x80 + 256) { /* �L�[�R�[�h�𕶎��R�[�h�ɕϊ� */
+					if (key_shift == 0) {
+						s[0] = keytable0[i - 256];
+					} else {
+						s[0] = keytable1[i - 256];
+					}
+				} else {
+					s[0] = 0;
+				}
+				if ('A' <= s[0] && s[0] <= 'Z') {	/* ���͕������A���t�@�x�b�g */
+					if (((key_leds & 4) == 0 && key_shift == 0) ||
+							((key_leds & 4) != 0 && key_shift != 0)) {
+						s[0] += 0x20;	/* �啶�����������ɕϊ� */
+					}
+				}
+				if (s[0] != 0) { /* �ʏ핶�� */
+					if (key_to == 0) {	/* �^�X�NA�� */
+						if (cursor_x < 128) {
+							/* �ꕶ���\�����Ă����A�J�[�\����1�i�߂� */
+							s[1] = 0;
+							putfonts8_asc_sht(sht_win, cursor_x, 28, COL8_000000, COL8_FFFFFF, s, 1);
+							cursor_x += 8;
+						}
+					} else {	/* �R���\�[���� */
+						fifo32_put(&task_cons->fifo, s[0] + 256);
+					}
+				}
+				if (i == 256 + 0x0e) {	/* �o�b�N�X�y�[�X */
+					if (key_to == 0) {	/* �^�X�NA�� */
+						if (cursor_x > 8) {
+							/* �J�[�\�����X�y�[�X�ŏ����Ă����A�J�[�\����1�߂� */
+							putfonts8_asc_sht(sht_win, cursor_x, 28, COL8_000000, COL8_FFFFFF, " ", 1);
+							cursor_x -= 8;
+						}
+					} else {	/* �R���\�[���� */
+						fifo32_put(&task_cons->fifo, 8 + 256);
+					}
+				}
+				if (i == 256 + 0x1c) {	/* Enter */
+					if (key_to != 0) {	/* �R���\�[���� */
+						fifo32_put(&task_cons->fifo, 10 + 256);
+					}
+				}
+				if (i == 256 + 0x0f) {	/* Tab */
+					if (key_to == 0) {
+						key_to = 1;
+						make_wtitle8(buf_win,  sht_win->bxsize,  "task_a",  0);
+						make_wtitle8(buf_cons, sht_cons->bxsize, "console", 1);
+						cursor_c = -1; /* �J�[�\�������� */
+						boxfill8(sht_win->buf, sht_win->bxsize, COL8_FFFFFF, cursor_x, 28, cursor_x + 7, 43);
+						fifo32_put(&task_cons->fifo, 2); /* �R���\�[���̃J�[�\��ON */
+					} else {
+						key_to = 0;
+						make_wtitle8(buf_win,  sht_win->bxsize,  "task_a",  1);
+						make_wtitle8(buf_cons, sht_cons->bxsize, "console", 0);
+						cursor_c = COL8_000000; /* �J�[�\�����o�� */
+						fifo32_put(&task_cons->fifo, 3); /* �R���\�[���̃J�[�\��OFF */
+					}
+					sheet_refresh(sht_win,  0, 0, sht_win->bxsize,  21);
+					sheet_refresh(sht_cons, 0, 0, sht_cons->bxsize, 21);
+				}
+				if (i == 256 + 0x2a) {	/* ���V�t�g ON */
+					key_shift |= 1;
+				}
+				if (i == 256 + 0x36) {	/* �E�V�t�g ON */
+					key_shift |= 2;
+				}
+				if (i == 256 + 0xaa) {	/* ���V�t�g OFF */
+					key_shift &= ~1;
+				}
+				if (i == 256 + 0xb6) {	/* �E�V�t�g OFF */
+					key_shift &= ~2;
+				}
+				if (i == 256 + 0x3a) {	/* CapsLock */
+					key_leds ^= 4;
+					fifo32_put(&keycmd, KEYCMD_LED);
+					fifo32_put(&keycmd, key_leds);
+				}
+				if (i == 256 + 0x45) {	/* NumLock */
+					key_leds ^= 2;
+					fifo32_put(&keycmd, KEYCMD_LED);
+					fifo32_put(&keycmd, key_leds);
+				}
+				if (i == 256 + 0x46) {	/* ScrollLock */
+					key_leds ^= 1;
+					fifo32_put(&keycmd, KEYCMD_LED);
+					fifo32_put(&keycmd, key_leds);
+				}
+				if (i == 256 + 0xfa) {	/* �L�[�{�[�h���f�[�^�𖳎��Ɏ󂯎����� */
+					keycmd_wait = -1;
+				}
+				if (i == 256 + 0xfe) {	/* �L�[�{�[�h���f�[�^�𖳎��Ɏ󂯎����Ȃ����� */
+					wait_KBC_sendready();
+					io_out8(PORT_KEYDAT, keycmd_wait);
+				}
+				/* �J�[�\���̍ĕ\�� */
+				if (cursor_c >= 0) {
+					boxfill8(sht_win->buf, sht_win->bxsize, cursor_c, cursor_x, 28, cursor_x + 7, 43);
+				}
+				sheet_refresh(sht_win, cursor_x, 28, cursor_x + 8, 44);
+			} else if (512 <= i && i <= 767) { /* �}�E�X�f�[�^ */
+				if (mouse_decode(&mdec, i - 512) != 0) {
+					/* �}�E�X�J�[�\���̈ړ� */
+					mx += mdec.x;
+					my += mdec.y;
+					if (mx < 0) {
+						mx = 0;
+					}
+					if (my < 0) {
+						my = 0;
+					}
+					if (mx > binfo->scrnx - 1) {
+						mx = binfo->scrnx - 1;
+					}
+					if (my > binfo->scrny - 1) {
+						my = binfo->scrny - 1;
+					}
+					sheet_slide(sht_mouse, mx, my);
+					if ((mdec.btn & 0x01) != 0) {
+						/* ���{�^���������Ă������Asht_win�𓮂��� */
+						sheet_slide(sht_win, mx - 80, my - 8);
+					}
+				}
+			} else if (i <= 1) { /* �J�[�\���p�^�C�} */
+				if (i != 0) {
+					timer_init(timer, &fifo, 0); /* ����0�� */
+					if (cursor_c >= 0) {
+						cursor_c = COL8_000000;
+					}
+				} else {
+					timer_init(timer, &fifo, 1); /* ����1�� */
+					if (cursor_c >= 0) {
+						cursor_c = COL8_FFFFFF;
+					}
+				}
+				timer_settime(timer, 50);
+				if (cursor_c >= 0) {
+					boxfill8(sht_win->buf, sht_win->bxsize, cursor_c, cursor_x, 28, cursor_x + 7, 43);
+					sheet_refresh(sht_win, cursor_x, 28, cursor_x + 8, 44);
+				}
+			}
+		}
+	}
+}
---
+++ 21day/console.c
@@ -0,0 +1,335 @@
+/* �R���\�[���֌W */
+
+#include "bootpack.h"
+#include <stdio.h>
+#include <string.h>
+
+void console_task(struct SHEET *sheet, unsigned int memtotal)
+{
+	struct TIMER *timer;
+	struct TASK *task = task_now();
+	struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
+	int i, fifobuf[128], *fat = (int *) memman_alloc_4k(memman, 4 * 2880);
+	struct CONSOLE cons;
+	char cmdline[30];
+	cons.sht = sheet;
+	cons.cur_x =  8;
+	cons.cur_y = 28;
+	cons.cur_c = -1;
+	*((int *) 0x0fec) = (int) &cons;
+
+	fifo32_init(&task->fifo, 128, fifobuf, task);
+	timer = timer_alloc();
+	timer_init(timer, &task->fifo, 1);
+	timer_settime(timer, 50);
+	file_readfat(fat, (unsigned char *) (ADR_DISKIMG + 0x000200));
+
+	/* �v�����v�g�\�� */
+	cons_putchar(&cons, '>', 1);
+
+	for (;;) {
+		io_cli();
+		if (fifo32_status(&task->fifo) == 0) {
+			task_sleep(task);
+			io_sti();
+		} else {
+			i = fifo32_get(&task->fifo);
+			io_sti();
+			if (i <= 1) { /* �J�[�\���p�^�C�} */
+				if (i != 0) {
+					timer_init(timer, &task->fifo, 0); /* ����0�� */
+					if (cons.cur_c >= 0) {
+						cons.cur_c = COL8_FFFFFF;
+					}
+				} else {
+					timer_init(timer, &task->fifo, 1); /* ����1�� */
+					if (cons.cur_c >= 0) {
+						cons.cur_c = COL8_000000;
+					}
+				}
+				timer_settime(timer, 50);
+			}
+			if (i == 2) {	/* �J�[�\��ON */
+				cons.cur_c = COL8_FFFFFF;
+			}
+			if (i == 3) {	/* �J�[�\��OFF */
+				boxfill8(sheet->buf, sheet->bxsize, COL8_000000, cons.cur_x, cons.cur_y, cons.cur_x + 7, cons.cur_y + 15);
+				cons.cur_c = -1;
+			}
+			if (256 <= i && i <= 511) { /* �L�[�{�[�h�f�[�^�i�^�X�NA�o�R�j */
+				if (i == 8 + 256) {
+					/* �o�b�N�X�y�[�X */
+					if (cons.cur_x > 16) {
+						/* �J�[�\�����X�y�[�X�ŏ����Ă����A�J�[�\����1�߂� */
+						cons_putchar(&cons, ' ', 0);
+						cons.cur_x -= 8;
+					}
+				} else if (i == 10 + 256) {
+					/* Enter */
+					/* �J�[�\�����X�y�[�X�ŏ����Ă������s���� */
+					cons_putchar(&cons, ' ', 0);
+					cmdline[cons.cur_x / 8 - 2] = 0;
+					cons_newline(&cons);
+					cons_runcmd(cmdline, &cons, fat, memtotal);	/* �R�}���h���s */
+					/* �v�����v�g�\�� */
+					cons_putchar(&cons, '>', 1);
+				} else {
+					/* ���ʕ��� */
+					if (cons.cur_x < 240) {
+						/* �ꕶ���\�����Ă����A�J�[�\����1�i�߂� */
+						cmdline[cons.cur_x / 8 - 2] = i - 256;
+						cons_putchar(&cons, i - 256, 1);
+					}
+				}
+			}
+			/* �J�[�\���ĕ\�� */
+			if (cons.cur_c >= 0) {
+				boxfill8(sheet->buf, sheet->bxsize, cons.cur_c, cons.cur_x, cons.cur_y, cons.cur_x + 7, cons.cur_y + 15);
+			}
+			sheet_refresh(sheet, cons.cur_x, cons.cur_y, cons.cur_x + 8, cons.cur_y + 16);
+		}
+	}
+}
+
+void cons_putchar(struct CONSOLE *cons, int chr, char move)
+{
+	char s[2];
+	s[0] = chr;
+	s[1] = 0;
+	if (s[0] == 0x09) {	/* �^�u */
+		for (;;) {
+			putfonts8_asc_sht(cons->sht, cons->cur_x, cons->cur_y, COL8_FFFFFF, COL8_000000, " ", 1);
+			cons->cur_x += 8;
+			if (cons->cur_x == 8 + 240) {
+				cons_newline(cons);
+			}
+			if (((cons->cur_x - 8) & 0x1f) == 0) {
+				break;	/* 32�Ŋ����؂ꂽ��break */
+			}
+		}
+	} else if (s[0] == 0x0a) {	/* ���s */
+		cons_newline(cons);
+	} else if (s[0] == 0x0d) {	/* ���A */
+		/* �Ƃ肠�����Ȃɂ����Ȃ� */
+	} else {	/* ���ʂ̕��� */
+		putfonts8_asc_sht(cons->sht, cons->cur_x, cons->cur_y, COL8_FFFFFF, COL8_000000, s, 1);
+		if (move != 0) {
+			/* move��0�̂Ƃ��̓J�[�\�����i�߂Ȃ� */
+			cons->cur_x += 8;
+			if (cons->cur_x == 8 + 240) {
+				cons_newline(cons);
+			}
+		}
+	}
+	return;
+}
+
+void cons_newline(struct CONSOLE *cons)
+{
+	int x, y;
+	struct SHEET *sheet = cons->sht;
+	if (cons->cur_y < 28 + 112) {
+		cons->cur_y += 16; /* ���̍s�� */
+	} else {
+		/* �X�N���[�� */
+		for (y = 28; y < 28 + 112; y++) {
+			for (x = 8; x < 8 + 240; x++) {
+				sheet->buf[x + y * sheet->bxsize] = sheet->buf[x + (y + 16) * sheet->bxsize];
+			}
+		}
+		for (y = 28 + 112; y < 28 + 128; y++) {
+			for (x = 8; x < 8 + 240; x++) {
+				sheet->buf[x + y * sheet->bxsize] = COL8_000000;
+			}
+		}
+		sheet_refresh(sheet, 8, 28, 8 + 240, 28 + 128);
+	}
+	cons->cur_x = 8;
+	return;
+}
+
+void cons_putstr0(struct CONSOLE *cons, char *s)
+{
+	for (; *s != 0; s++) {
+		cons_putchar(cons, *s, 1);
+	}
+	return;
+}
+
+void cons_putstr1(struct CONSOLE *cons, char *s, int l)
+{
+	int i;
+	for (i = 0; i < l; i++) {
+		cons_putchar(cons, s[i], 1);
+	}
+	return;
+}
+
+void cons_runcmd(char *cmdline, struct CONSOLE *cons, int *fat, unsigned int memtotal)
+{
+	if (strcmp(cmdline, "mem") == 0) {
+		cmd_mem(cons, memtotal);
+	} else if (strcmp(cmdline, "cls") == 0) {
+		cmd_cls(cons);
+	} else if (strcmp(cmdline, "dir") == 0) {
+		cmd_dir(cons);
+	} else if (strncmp(cmdline, "type ", 5) == 0) {
+		cmd_type(cons, fat, cmdline);
+	} else if (cmdline[0] != 0) {
+		if (cmd_app(cons, fat, cmdline) == 0) {
+			/* �R�}���h�ł͂Ȃ��A�A�v���ł��Ȃ��A�����ɋ��s�ł��Ȃ� */
+			cons_putstr0(cons, "Bad command.\n\n");
+		}
+	}
+	return;
+}
+
+void cmd_mem(struct CONSOLE *cons, unsigned int memtotal)
+{
+	struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
+	char s[60];
+	sprintf(s, "total   %dMB\nfree %dKB\n\n", memtotal / (1024 * 1024), memman_total(memman) / 1024);
+	cons_putstr0(cons, s);
+	return;
+}
+
+void cmd_cls(struct CONSOLE *cons)
+{
+	int x, y;
+	struct SHEET *sheet = cons->sht;
+	for (y = 28; y < 28 + 128; y++) {
+		for (x = 8; x < 8 + 240; x++) {
+			sheet->buf[x + y * sheet->bxsize] = COL8_000000;
+		}
+	}
+	sheet_refresh(sheet, 8, 28, 8 + 240, 28 + 128);
+	cons->cur_y = 28;
+	return;
+}
+
+void cmd_dir(struct CONSOLE *cons)
+{
+	struct FILEINFO *finfo = (struct FILEINFO *) (ADR_DISKIMG + 0x002600);
+	int i, j;
+	char s[30];
+	for (i = 0; i < 224; i++) {
+		if (finfo[i].name[0] == 0x00) {
+			break;
+		}
+		if (finfo[i].name[0] != 0xe5) {
+			if ((finfo[i].type & 0x18) == 0) {
+				sprintf(s, "filename.ext   %7d\n", finfo[i].size);
+				for (j = 0; j < 8; j++) {
+					s[j] = finfo[i].name[j];
+				}
+				s[ 9] = finfo[i].ext[0];
+				s[10] = finfo[i].ext[1];
+				s[11] = finfo[i].ext[2];
+				cons_putstr0(cons, s);
+			}
+		}
+	}
+	cons_newline(cons);
+	return;
+}
+
+void cmd_type(struct CONSOLE *cons, int *fat, char *cmdline)
+{
+	struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
+	struct FILEINFO *finfo = file_search(cmdline + 5, (struct FILEINFO *) (ADR_DISKIMG + 0x002600), 224);
+	char *p;
+	if (finfo != 0) {
+		/* �t�@�C�������������ꍇ */
+		p = (char *) memman_alloc_4k(memman, finfo->size);
+		file_loadfile(finfo->clustno, finfo->size, p, fat, (char *) (ADR_DISKIMG + 0x003e00));
+		cons_putstr1(cons, p, finfo->size);
+		memman_free_4k(memman, (int) p, finfo->size);
+	} else {
+		/* �t�@�C�����������Ȃ������ꍇ */
+		cons_putstr0(cons, "File not found.\n");
+	}
+	cons_newline(cons);
+	return;
+}
+
+int cmd_app(struct CONSOLE *cons, int *fat, char *cmdline)
+{
+	struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
+	struct FILEINFO *finfo;
+	struct SEGMENT_DESCRIPTOR *gdt = (struct SEGMENT_DESCRIPTOR *) ADR_GDT;
+	char name[18], *p, *q;
+	struct TASK *task = task_now();
+	int i;
+
+	/* �R�}���h���C�������t�@�C�����𐶐� */
+	for (i = 0; i < 13; i++) {
+		if (cmdline[i] <= ' ') {
+			break;
+		}
+		name[i] = cmdline[i];
+	}
+	name[i] = 0; /* �Ƃ肠�����t�@�C�����̌�����0�ɂ��� */
+
+	/* �t�@�C�����T�� */
+	finfo = file_search(name, (struct FILEINFO *) (ADR_DISKIMG + 0x002600), 224);
+	if (finfo == 0 && name[i - 1] != '.') {
+		/* �������Ȃ������̂Ō�����".HRB"�����Ă������x�T���Ă݂� */
+		name[i    ] = '.';
+		name[i + 1] = 'H';
+		name[i + 2] = 'R';
+		name[i + 3] = 'B';
+		name[i + 4] = 0;
+		finfo = file_search(name, (struct FILEINFO *) (ADR_DISKIMG + 0x002600), 224);
+	}
+
+	if (finfo != 0) {
+		/* 找到文件的情况 */
+		p = (char *) memman_alloc_4k(memman, finfo->size);
+		q = (char *) memman_alloc_4k(memman, 64 * 1024);//应用程序专用内存64k
+		*((int *) 0xfe8) = (int) p;//将1003段的段基址设为（0xfe8）
+		file_loadfile(finfo->clustno, finfo->size, p, fat, (char *) (ADR_DISKIMG + 0x003e00));
+		set_segmdesc(gdt + 1003, finfo->size - 1, (int) p, AR_CODE32_ER + 0x60);
+		set_segmdesc(gdt + 1004, 64 * 1024 - 1,   (int) q, AR_DATA32_RW + 0x60);
+		if (finfo->size >= 8 && strncmp(p + 4, "Hari", 4) == 0) { //如果是c编写的应用程序
+			p[0] = 0xe8;//这些二进制代码的功能是调用C文件的main函数
+			p[1] = 0x16;
+			p[2] = 0x00;
+			p[3] = 0x00;
+			p[4] = 0x00;
+			p[5] = 0xcb;
+		}
+		//void start_app(int eip, int cs, int esp, int ds, int *tss_esp0);
+		start_app(0, 1003 * 8, 64 * 1024, 1004 * 8, &(task->tss.esp0));
+		memman_free_4k(memman, (int) p, finfo->size);
+		memman_free_4k(memman, (int) q, 64 * 1024);
+		cons_newline(cons);
+		return 1;
+	}
+	/* 没有找到文件的情况 */
+	return 0;
+}
+
+int *hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int eax)
+{
+	int cs_base = *((int *) 0xfe8);
+	struct TASK *task = task_now();
+	struct CONSOLE *cons = (struct CONSOLE *) *((int *) 0x0fec);
+	if (edx == 1) {
+		cons_putchar(cons, eax & 0xff, 1);
+	} else if (edx == 2) {
+		cons_putstr0(cons, (char *) ebx + cs_base);//段基址加上ebx的地址
+	} else if (edx == 3) {
+		cons_putstr1(cons, (char *) ebx + cs_base, ecx);
+	} else if (edx == 4) {
+		return &(task->tss.esp0);
+	}
+	return 0;
+}
+
+int *inthandler0d(int *esp)
+{
+	struct CONSOLE *cons = (struct CONSOLE *) *((int *) 0x0fec);
+	struct TASK *task = task_now();
+	cons_putstr0(cons, "\nINT 0D :\n General Protected Exception.\n");
+	return &(task->tss.esp0);	/* 让程序强制结束 */
+}
